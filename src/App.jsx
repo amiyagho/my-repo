92ab847540651144b0c41909749558d0bc125907
amiyagho <ghoshamiya950@gmail.com>
2023-08-09 22:54:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    // <div className='  text-justify'>App</div>
+    <main className=' w-full h-screen flex justify-center '>
+        <div className="">App</div>
+    </main>
+  )
+}
+
+export default App
